546d855c653d05af57e33e74a108d10cc396c44d
hjyeeoonng <100748980+hjyeeoonng@users.noreply.github.com>
2023-09-25 17:31:44 +0900

    Style: modify JoinPageHelper style

--- frontend/src/pages/JoinPageHelper.jsx
+++ frontend/src/pages/JoinPageHelper.jsx
@@ -1,9 +1,89 @@
 import styled from "styled-components";
 import React from 'react';
+import Header from "../components/Header";
+
+const Root = styled.div`
+width: 100vw;
+height:100vh;
+background: #FFF8F3;
+display: flex;
+flex-direction: column;
+justify-content: flex-start;
+align-items: center;
+`
+
+const JoinHInputBox = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-between;
+width : 600px;
+height : 70px;
+margin: 6px auto;
+`
+
+const JoinHInput = styled.input`
+width:70%;
+height: 50px;
+background: #FFF;
+border: 1px solid #BB6C25;
+border-radius: 20px;
+`
+
+const JoinHText = styled.p`
+witdth: 30%;
+color: #000;
+font-family: Noto Serif KR;
+font-size: 32px;
+font-style: normal;
+font-weight: 600;
+line-height: 48px; /* 150% */
+`
+const JoinHBtn = styled.div`
+width : 600px;
+height : 80px;
+border-radius: 20px;
+background: #BB6C25;
+
+display: flex;
+align-items: center;
+justify-content:center;
+margin: 50px auto;
+
+color: #FFF;
+text-align: center;
+font-family: Roboto Flex;
+font-size: 40px;
+font-style: normal;
+font-weight: 800;
+line-height: normal;
+`
 
 export const JoinPageHelper = () =>{
     return (
-        <></>
+        <Root>
+            <Header></Header>
+            <div style={{height:'100%',display:'flex',flexDirection:'column',justifyContent:'space-between',alignItems:'center'}}>
+                <div style={{margin: '50px auto auto auto'}}>
+                    <JoinHInputBox>
+                        <JoinHText>이름</JoinHText>
+                        <JoinHInput></JoinHInput>
+                    </JoinHInputBox>
+                    <JoinHInputBox>
+                        <JoinHText>아이디</JoinHText>
+                        <JoinHInput></JoinHInput>
+                    </JoinHInputBox>
+                    <JoinHInputBox>
+                        <JoinHText>비밀번호</JoinHText>
+                        <JoinHInput></JoinHInput>
+                    </JoinHInputBox>
+                    <JoinHInputBox>
+                        <JoinHText>전화번호</JoinHText>
+                        <JoinHInput></JoinHInput>
+                    </JoinHInputBox>
+                </div>
+                <JoinHBtn>회원가입</JoinHBtn>
+            </div>
+        </Root>
     );
 };
 export default JoinPageHelper;
