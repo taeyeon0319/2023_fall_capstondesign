b83367822ef9925cc7335eb2a81a11ede71787e7
hjyeeoonng <100748980+hjyeeoonng@users.noreply.github.com>
2023-09-25 15:00:34 +0900

    Feat: create joinPage

---
+++ frontend/src/pages/JoinPage.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import React from 'react';
+
+const Root = styled.div`
+width: 100%;
+background-color: #FFF;
+display: flex;
+justify-content: flex-end;
+`
+
+export const JoinPage = () => {
+    return(
+        <Root>
+        </Root>
+    );
+};
+export default JoinPage;
