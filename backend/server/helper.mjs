6c160c89168692b3d4e81d49022dcdb410893376
taeyeon0319 <xodus1349@likelion.org>
2023-10-10 15:34:47 +0900

    [김태연]백엔드 api기초

--- backend/server/helper.mjs
+++ backend/server/helper.mjs
@@ -7,4 +7,9 @@ helperRouter.get('/', (req, res) => {
     res.json("success도우미");
 });
 
+//user가상 데이터 테이블 설계에 고민이 필요할듯..!!
+// /user :user 정보 출력 api(가상 테이블 데이터 출력)
+
+
+
 export default helperRouter;
